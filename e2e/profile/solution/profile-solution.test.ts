42662e01667326392e19c4e79fba5f7de643080a
Philipp Kaiser <philipp.kaiser@frachtwerk.de>
2023-11-30 08:48:47 +0100

    add description

--- e2e/profile/solution/profile-solution.test.ts
+++ e2e/profile/solution/profile-solution.test.ts
@@ -1,5 +1,5 @@
-import { test, expect } from '@playwright/test'
-import { TEST_USER, BASE_URL } from '../../../playwright.config'
+import { test, expect } from "@playwright/test";
+import { TEST_USER, BASE_URL } from "../../../playwright.config";
 
 /*
 Tests:
@@ -7,57 +7,61 @@ Tests:
 - login, follow user, unfollow user
 */
 
-test.describe('Edit user', () => {
+test.describe("Edit user", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto(BASE_URL)
+    await page.goto(BASE_URL);
 
-    await page.getByText('Login').click()
-    await page.getByPlaceholder('Email').fill(TEST_USER.mail)
-    await page.getByPlaceholder('Password').fill(TEST_USER.password)
+    await page.getByText("Login").click();
+    await page.getByPlaceholder("Email").fill(TEST_USER.mail);
+    await page.getByPlaceholder("Password").fill(TEST_USER.password);
 
-    await expect(page.getByText('Sign In')).toBeVisible()
+    await expect(page.getByText("Sign In")).toBeVisible();
 
-    await page.getByText('Sign In').click()
+    await page.getByText("Sign In").click();
 
-    await expect(page.getByText('Logout')).toBeVisible()
-  })
+    await expect(page.getByText("Logout")).toBeVisible();
+  });
 
-  test('Edit user', async ({ page }) => {
-    await page.getByText('Profile').click()
-    await page.getByText('Edit').click()
+  test("It should edit the test user", async ({ page }) => {
+    await page.getByText("Profile").click();
+    await page.getByText("Edit").click();
 
-    await expect(page.getByText('Save')).toBeVisible()
+    await expect(page.getByText("Save")).toBeVisible();
 
-    await page.getByPlaceholder('Name', { exact: true }).fill('Test User Edited')
-    await page.getByPlaceholder('Username').fill('Test Moin')
-    await page.getByRole('button', { name: 'Save' }).click()
+    await page
+      .getByPlaceholder("Name", { exact: true })
+      .fill("Test User Edited");
+    await page.getByPlaceholder("Username").fill("Test Moin");
+    await page.getByRole("button", { name: "Save" }).click();
 
-    await expect(page.getByPlaceholder('Name', { exact: true })).toHaveValue('Test User Edited')
-    await expect(page.getByPlaceholder('Username')).toHaveValue('Test Moin')
+    await expect(page.getByPlaceholder("Name", { exact: true })).toHaveValue(
+      "Test User Edited"
+    );
+    await expect(page.getByPlaceholder("Username")).toHaveValue("Test Moin");
 
-    await page.getByPlaceholder('Name', { exact: true }).fill(TEST_USER.name)
-    await page.getByPlaceholder('Username').fill(TEST_USER.username)
-  })
+    await page.getByPlaceholder("Name", { exact: true }).fill(TEST_USER.name);
+    await page.getByPlaceholder("Username").fill(TEST_USER.username);
+  });
 
-  test('Follow user', async ({ page }) => {
-    await page.getByText('Gerrit').click()
+  test("It should follow a user named Gerrit", async ({ page }) => {
+    await page.getByText("Gerrit").click();
 
-    await expect(page.getByText('Follow')).toBeVisible()
+    await expect(page.getByText("Follow")).toBeVisible();
 
-    await page.getByText('Follow').click()
+    await page.getByText("Follow").click();
 
-    await expect(page.getByText('Success')).toBeVisible()
-    await expect(page.getByText('Unfollow')).toBeVisible()
-  })
+    await expect(page.getByText("Success")).toBeVisible();
+    await expect(page.getByText("Unfollow")).toBeVisible();
+  });
 
-  test('Unfollow user', async ({ page }) => {
-    await page.getByText('Gerrit').click()
+  test("It should unfollow a user named Gerrit", async ({ page }) => {
+    await page.getByText("Gerrit").click();
 
-    await expect(page.getByText('Unfollow')).toBeVisible()
+    await expect(page.getByText("Unfollow")).toBeVisible();
 
-    await page.getByText('Unfollow').click()
+    await page.getByText("Unfollow").click();
 
-    await expect(page.getByText('Success')).toBeVisible()
-    await expect(page.getByText('Follow')).toBeVisible()
-  })
-})
+    await expect(page.getByText("Success")).toBeVisible();
+    await expect(page.getByText("Follow")).toBeVisible();
+  });
+});
--- e2e/tweet/solution/tweet-solution.test.ts
+++ e2e/tweet/solution/tweet-solution.test.ts
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test'
-import { TEST_USER, BASE_URL } from '../../../playwright.config'
+import { test, expect } from "@playwright/test";
+import { TEST_USER, BASE_URL } from "../../../playwright.config";
 
-const TEST_TWEET = 'Test tweet'
+const TEST_TWEET = "Test tweet";
 
 /*
 Tests:
@@ -9,26 +9,26 @@ Tests:
 - login, comment a tweet, like a tweet
 */
 
-test.describe('Create and like tweet', () => {
+test.describe("Create a tweet", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto(BASE_URL)
+    await page.goto(BASE_URL);
 
-    await page.getByText('Login').click()
-    await page.getByPlaceholder('Email').fill(TEST_USER.mail)
-    await page.getByPlaceholder('Password').fill(TEST_USER.password)
+    await page.getByText("Login").click();
+    await page.getByPlaceholder("Email").fill(TEST_USER.mail);
+    await page.getByPlaceholder("Password").fill(TEST_USER.password);
 
-    await page.getByText('Sign In').click()
+    await page.getByText("Sign In").click();
 
-    await expect(page.getByText('Logout')).toBeVisible()
-  })
+    await expect(page.getByText("Logout")).toBeVisible();
+  });
 
-  test('Create new tweet', async ({ page }) => {
-    await page.getByText('Tweet').nth(0).click()
-    await page.getByPlaceholder("What's happening?").fill(TEST_TWEET)
-    await page.getByRole('button', { name: 'Tweet' }).click()
+  test("It should create new tweet", async ({ page }) => {
+    await page.getByText("Tweet").nth(0).click();
+    await page.getByPlaceholder("What's happening?").fill(TEST_TWEET);
+    await page.getByRole("button", { name: "Tweet" }).click();
 
-    await expect(page.getByText('Tweet created')).toBeVisible()
+    await expect(page.getByText("Tweet created")).toBeVisible();
 
-    await expect(page.getByText('Test tweet').first()).toBeVisible()
-  })
-})
+    await expect(page.getByText("Test tweet").first()).toBeVisible();
+  });
+});
